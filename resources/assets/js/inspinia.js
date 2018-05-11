43c7e788f87878da00e0364bd9448851cf430801
Alucarth <ltorrezs2008@gmail.com>
2018-05-11 12:07:12 -0400

    add template

---
+++ resources/assets/js/inspinia.js
@@ -0,0 +1,29 @@
+
+try {
+  window.$ = window.jQuery = require('jquery'); //siempre usar el de Node Packge Manager
+
+  require('../inspinia/js/bootstrap.min.js');
+
+} catch (e) { }
+
+
+require('../inspinia/js/inspinia.js');
+require('./pace.min.js');
+require('../inspinia/js/plugins/slimscroll/jquery.slimscroll.min.js');
+require('../inspinia/js/plugins/chosen/chosen.jquery.js');
+require('../inspinia/js/plugins/jsKnob/jquery.knob.js');
+require('../inspinia/js/plugins/jasny/jasny-bootstrap.min.js');
+require('../inspinia/js/plugins/datapicker/bootstrap-datepicker.js');
+require('../inspinia/js/plugins/nouslider/jquery.nouislider.min.js');
+require('../inspinia/js/plugins/switchery/switchery.js');
+require('../inspinia/js/plugins/ionRangeSlider/ion.rangeSlider.min.js');
+require('../inspinia/js/plugins/iCheck/icheck.min.js');
+require('../inspinia/js/plugins/metisMenu/jquery.metisMenu.js');
+require('../inspinia/js/plugins/colorpicker/bootstrap-colorpicker.min.js');
+// require('../inspinia/js/plugins/clockpicker/clockpicker.js');
+// require('../inspinia/js/plugins/cropper/cropper.min.js');
+// require('moment');
+require('../inspinia/js/plugins/select2/select2.full.min.js');
+// require('../inspinia/js/plugins/touchspin/jquery.bootstrap-touchspin.min.js');
+// require('../inspinia/js/plugins/bootstrap-tagsinput/bootstrap-tagsinput.js');
+// require('../inspinia/js/plugins/dualListbox/jquery.bootstrap-duallistbox.js');
