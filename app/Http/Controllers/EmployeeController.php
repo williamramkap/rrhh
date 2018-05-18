0557a271c6dd304ae7265e3a1c2989e6b23bf422
Vrian7 <i-am-brian@gmail.com>
2018-05-18 11:36:20 -0400

    seeder correction

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -30,153 +30,153 @@ class EmployeeController extends Controller
         //return $discount[0]->discoun
         //return $discount;
 
-        // Excel::create('Filename', function($excel) {
-        //     // Set the title
-        //     $excel->setTitle('Our new awesome title');
+        Excel::create('Filename', function($excel) {
+            // Set the title
+            $excel->setTitle('Laboral');
 
-        //     // Chain the setters
-        //     $excel->setCreator('Maatwebsite')
-        //             ->setCompany('Maatwebsite');
+            // Chain the setters
+            $excel->setCreator('unidad de sistemas')
+                    ->setCompany('MUSERPOL');
 
-        //     // Call them separately
-        //     $excel->setDescription('A demonstration to change the file properties');
+            // Call them separately
+            $excel->setDescription('description');
             
-        //     $excel->sheet('Sheetname', function($sheet) {
-        //         $center_style = array(
-        //             'alignment' => array(
-        //                 'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
-        //             )
-        //         );
+            $excel->sheet('Laboral', function($sheet) {
+                $center_style = array(
+                    'alignment' => array(
+                        'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+                    )
+                );
 
 
-        //         $name = "MUTUAL DE SERVICIOS AL POLICIA";
-        //         $nit = "NIT 234578021";
-        //         $address = "Av. 6 De Agosto No. 2354 - Zona Sopocachi";
-        //         $title = "PLANILLA DE HABERES";
-        //         $subtitle = "PERSONAL EVENTUAL - MES  ABRIL DE 2018";
-        //         $exchange = "(EXPRESADO EN BOLIVIANOS)";
-        //         $sheet->mergeCells('A1:C1');
-        //         $sheet->mergeCells('A2:C2');
-        //         $sheet->mergeCells('A3:C3');
+                $name = "MUTUAL DE SERVICIOS AL POLICIA";
+                $nit = "NIT 234578021";
+                $address = "Av. 6 De Agosto No. 2354 - Zona Sopocachi";
+                $title = "PLANILLA DE HABERES";
+                $subtitle = "PERSONAL EVENTUAL - MES  ABRIL DE 2018";
+                $exchange = "(EXPRESADO EN BOLIVIANOS)";
+                $sheet->mergeCells('A1:C1');
+                $sheet->mergeCells('A2:C2');
+                $sheet->mergeCells('A3:C3');
 
-        //         $sheet->row(1, array(
-        //             $name
-        //         ));
-        //         $sheet->row(2, array(
-        //             $nit
-        //         ));
-        //         $sheet->row(3, array(
-        //             $address
-        //         ));
+                $sheet->row(1, array(
+                    $name
+                ));
+                $sheet->row(2, array(
+                    $nit
+                ));
+                $sheet->row(3, array(
+                    $address
+                ));
                 
-        //         $objDrawing = new PHPExcel_Worksheet_Drawing;
-        //         $objDrawing->setPath(public_path('images/logo.jpg')); //your image path
-        //         $objDrawing->setCoordinates('E1');                
-        //         $objDrawing->setHeight(74);                
-        //         $objDrawing->setWorksheet($sheet);
+                $objDrawing = new PHPExcel_Worksheet_Drawing;
+                $objDrawing->setPath(public_path('images/logo.jpg')); //your image path
+                $objDrawing->setCoordinates('E1');                
+                $objDrawing->setHeight(74);                
+                $objDrawing->setWorksheet($sheet);
 
-        //         $sheet->mergeCells('A5:Z5');
-        //         $sheet->getStyle("A5:Z5")->applyFromArray($center_style);
-        //         $sheet->mergeCells('A6:Z6');
-        //         $sheet->getStyle("A6:Z6")->applyFromArray($center_style);
-        //         $sheet->mergeCells('A7:Z7');
-        //         $sheet->getStyle("A7:Z7")->applyFromArray($center_style);
+                $sheet->mergeCells('A5:Z5');
+                $sheet->getStyle("A5:Z5")->applyFromArray($center_style);
+                $sheet->mergeCells('A6:Z6');
+                $sheet->getStyle("A6:Z6")->applyFromArray($center_style);
+                $sheet->mergeCells('A7:Z7');
+                $sheet->getStyle("A7:Z7")->applyFromArray($center_style);
 
 
 
-        //         $sheet->row(5, array(
-        //             $title
-        //         ));
-        //         $sheet->row(6, array(
-        //             $subtitle
-        //         ));
-        //         $sheet->row(7, array(
-        //             $exchange
-        //         ));
+                $sheet->row(5, array(
+                    $title
+                ));
+                $sheet->row(6, array(
+                    $subtitle
+                ));
+                $sheet->row(7, array(
+                    $exchange
+                ));
 
-        //         $row = 10;
-        //         $sheet->getStyle($row)->getFill()
-        //         ->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
-        //         ->getStartColor()
-        //         ->setARGB('FF808080');
-        //         $da = [
-        //             'Renta vejez 10%',
-        //             'Riesgo común 1,71%',
-        //             'Comisión 0,5%',
-        //             'Aporte solidario del asegurado 0,5%',
-        //             'Aporte Nacional solidario 1%, 5%, 10%',
-        //         ];
-        //         $sheet->row($row++, array(
-        //             'Nº',
-        //             'C.I.',
-        //             'TRABAJADOR',
-        //             'SUCURSAL',
-        //             'CUENTA BANCO UNION',          
-        //             'FECHA NACIMIENTO',
-        //             'SEXO',
-        //             'CARGO',
-        //             'PUESTO',
-        //             'FECHA DE INGRESO',
-        //             'FECHA VENCIMIENTO CONTRATO',
-        //             'DIAS TRABAJADOS',
-        //             'HABER BASICO',
-        //             'TOTAL GANADO',
-        //             'AFP',                    
-        //             'Renta vejez 10%',
-        //             'Riesgo común 1,71%',
-        //             'Comisión 0,5%',
-        //             'Aporte solidario del asegurado 0,5%',
-        //             'Aporte Nacional solidario 1%, 5%, 10%',
-        //             'TOTAL DESCUENTOS DE LEY',
-        //             'SUELDO NETO',
-        //             'RC-IVA 13%',
-        //             'Desc. Atrasos, Abandonos, Faltas y Licencia S/G Haberes',
-        //             'TOTAL DESCUENTOS',
-        //             'LIQUIDO PAGABLE',
-        //        ));               
-        //        $payrolls = Payroll::get();
-        //        $number = 1;
-        //        foreach($payrolls as $payroll){
+                $row = 10;
+                $sheet->getStyle($row)->getFill()
+                ->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
+                ->getStartColor()
+                ->setARGB('FF808080');
+                $da = [
+                    'Renta vejez 10%',
+                    'Riesgo común 1,71%',
+                    'Comisión 0,5%',
+                    'Aporte solidario del asegurado 0,5%',
+                    'Aporte Nacional solidario 1%, 5%, 10%',
+                ];
+                $sheet->row($row++, array(
+                    'Nº',
+                    'C.I.',
+                    'TRABAJADOR',
+                    'SUCURSAL',
+                    'CUENTA BANCO UNION',          
+                    'FECHA NACIMIENTO',
+                    'SEXO',
+                    'CARGO',
+                    'PUESTO',
+                    'FECHA DE INGRESO',
+                    'FECHA VENCIMIENTO CONTRATO',
+                    'DIAS TRABAJADOS',
+                    'HABER BASICO',
+                    'TOTAL GANADO',
+                    'AFP',                    
+                    'Renta vejez 10%',
+                    'Riesgo común 1,71%',
+                    'Comisión 0,5%',
+                    'Aporte solidario del asegurado 0,5%',
+                    'Aporte Nacional solidario 1%, 5%, 10%',
+                    'TOTAL DESCUENTOS DE LEY',
+                    'SUELDO NETO',
+                    'RC-IVA 13%',
+                    'Desc. Atrasos, Abandonos, Faltas y Licencia S/G Haberes',
+                    'TOTAL DESCUENTOS',
+                    'LIQUIDO PAGABLE',
+               ));               
+               $payrolls = Payroll::get();
+               $number = 1;
+               foreach($payrolls as $payroll){
 
-        //         $employee = $payroll->employee;
-        //         $contract = Contract::where('employee_id',$employee->id)->where('status',true)->first();
-        //         $position = Position::where('employee_id',$employee->id)->first();
-        //         $discount = DiscountPayroll::where('payroll_id',$payroll->id)->get();
-        //         //$i
-        //         $discount = DiscountPayroll::where('payroll_id',$payroll->id)->select('id','amount')->orderBy('discount_id','asc')->get()->pluck('amount','id');
-        //         $sheet->row($row++, array(
-        //             $number++,
-        //             $employee->identity_card,
-        //             $employee->last_name.' '.$employee->mothers_last_name.' '.$employee->first_name.' '.$employee->mothers_last_name,
-        //             $employee->group_job->name??'Central',
-        //             $employee->account_number,
-        //             date("d/m/Y", strtotime($employee->birth_date)),
-        //             $employee->gender,                    
-        //             $position->charge->name??'',
-        //             $position->name??'',
-        //             date("d/m/Y", strtotime($contract->date_start)),
-        //             date("d/m/Y", strtotime($contract->date_end)),                    
-        //             $payroll->worked_days,
-        //             $position->charge->base_wage ?? '0',
-        //             $payroll->quotable,
-        //             $employee->management_entity->name,
-        //             $discount[1] ?? '',
-        //             $discount[2] ?? '',
-        //             $discount[3] ?? '',
-        //             $discount[4] ?? '',
-        //             $discount[5] ?? '',
-        //             $payroll->total_amount_discount_institution,
-        //             $payroll->payable_liquid,
-        //             0,
-        //             $payroll->total_discounts,
-        //             $payroll->total_discounts,
-        //             $payroll->payable_liquid
-        //         ));
-        //        }
-        //     });
-        // })->download('xls');
+                $employee = $payroll->employee;
+                $contract = Contract::where('employee_id',$employee->id)->where('status',true)->first();
+                $position = Position::where('employee_id',$employee->id)->first();
+                $discount = DiscountPayroll::where('payroll_id',$payroll->id)->get();
+                //$i
+                $discount = DiscountPayroll::where('payroll_id',$payroll->id)->select('id','amount')->orderBy('discount_id','asc')->get()->pluck('amount','id');
+                $sheet->row($row++, array(
+                    $number++,
+                    $employee->identity_card,
+                    $employee->last_name.' '.$employee->mothers_last_name.' '.$employee->first_name.' '.$employee->mothers_last_name,
+                    $employee->group_job->name??'Central',
+                    $employee->account_number,
+                    date("d/m/Y", strtotime($employee->birth_date)),
+                    $employee->gender,                    
+                    $position->charge->name??'',
+                    $position->name??'',
+                    date("d/m/Y", strtotime($contract->date_start)),
+                    date("d/m/Y", strtotime($contract->date_end)),                    
+                    $payroll->worked_days,
+                    $position->charge->base_wage ?? '0',
+                    $payroll->quotable,
+                    $employee->management_entity->name,
+                    $discount[1] ?? '',
+                    $discount[2] ?? '',
+                    $discount[3] ?? '',
+                    $discount[4] ?? '',
+                    $discount[5] ?? '',
+                    $payroll->total_amount_discount_institution,
+                    $payroll->payable_liquid,
+                    0,
+                    $payroll->total_discounts,
+                    $payroll->total_discounts,
+                    $payroll->payable_liquid
+                ));
+               }
+            });
+        })->download('xls');
 
-        // return ;
+        return ;
         $employees = Employee::get();
         $data = [
             'employees' => $employees,
