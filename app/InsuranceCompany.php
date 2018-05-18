6d7d0cf977893bfc34fe064c426cf36c198c995a
Ale Guis F <aleguis@icloud.com>
2018-05-18 00:00:08 -0400

    bug fixes DB

--- app/InsuranceCompany.php
+++ app/InsuranceCompany.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class InsuranceCompany extends Model
 {
-    //
+    protected $table = "insurance_companies";
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
